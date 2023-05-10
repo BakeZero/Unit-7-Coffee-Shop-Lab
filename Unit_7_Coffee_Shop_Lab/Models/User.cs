92facf9dfd4d4f09cc99667be0234dc1c56d12b0
harry <harry.nguyen.2016@gmail.com>
2023-05-10 14:41:40 -0400

    first commit

---
+++ Unit_7_Coffee_Shop_Lab/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Unit_7_Coffee_Shop_Lab.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
